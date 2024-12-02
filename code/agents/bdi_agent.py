a4ecefe9e6ceb8e435ab995cc75a66df569292fd
levi-carvalho <lv.levicarvalho@gmail.com>
2024-12-01 22:17:42 -0300

    the agent now considers the number of moves from itself to the resource to the base and the value of the resource to define the target

--- code/agents/bdi_agent.py
+++ code/agents/bdi_agent.py
@@ -2,10 +2,21 @@ from settings import *
 from agents.utility_based import UtilityBased
 
 class BDIAgent(UtilityBased):
-    def __init__(self, size, position, sprite_sheet, groups, collision_sprites, objectives, utility_agents):
-        super().__init__(size, position, sprite_sheet, groups, collision_sprites, objectives, utility_agents)
+    def __init__(self, size, position, sprite_sheet, utility_agents, game):
+        super().__init__(size, position, sprite_sheet, utility_agents, game)
     
-    def calc_objective(self):
-        objectives = sorted(self.objectives, key = lambda objective: math.dist(objective.rect.center, self.base_pos)//objective.value)
-        objectives = [objective for objective in objectives if (pygame.time.get_ticks() - objective.last_tried) >= objective.cooldown_duration]
-        return objectives
+    def define_resources_priority(self):
+        current_time = pygame.time.get_ticks()
+        self.found_resources = sorted(
+            self.found_resources,
+            key=lambda r: (
+                not(current_time - r.last_tried) > r.cooldown_duration,
+                    (self.calculate_moves(self.m_position, r.m_position) +
+                    self.calculate_moves(self.game.base.m_position, r.m_position))/r.value
+                )
+            )
+        return              
+    # def calc_objective(self):
+    #     objectives = sorted(self.objectives, key = lambda objective: math.dist(objective.rect.center, self.base_pos)//objective.value)
+    #     objectives = [objective for objective in objectives if (pygame.time.get_ticks() - objective.last_tried) >= objective.cooldown_duration]
+    #     return objectives
