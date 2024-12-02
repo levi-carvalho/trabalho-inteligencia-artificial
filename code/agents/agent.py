3f9c76d560dedc138aa09b9acccd131bc0fc3323
levi-carvalho <lv.levicarvalho@gmail.com>
2024-12-01 22:11:32 -0300

    added a rectangle for perception  to all agents, speed control with the keyboard and the agent now uses pathfinding to move across the map

--- code/agents/agent.py
+++ code/agents/agent.py
@@ -1,24 +1,27 @@
 from settings import *
+from resources import Resource
+from sprites import *
 
 class Agent(pygame.sprite.Sprite):
-    def __init__(self, size, position, game, *agrs):
+    def __init__(self, size, position, spritesheet, game, *agrs):
         super().__init__(game.all_sprites)
         self.game = game
         self.size = size
-        self.spritesheet = self.game.surfaces['state_based']
+        self.spritesheet = spritesheet
         self.collision_sprites = game.collision_sprites
         self.base_pos = position
         self.frames = {'down': [], 'left': [], 'right': [], 'up': []}
         
         self.load_images()
         self.image = self.frames['down'][0]
+        self.state, self.frame_index = 'down', 0
         self.rect = self.image.get_frect(topleft = position)
         self.direction = pygame.Vector2((0,0))
+        self.speed = 400
+        self.points = 0
         
         self.hitbox = self.rect
         
-        self.speed = 100
-        self.state, self.frame_index = 'down', 0
         
         self.layer_order = 3
         
@@ -26,77 +29,121 @@ class Agent(pygame.sprite.Sprite):
         self.moving = False
         self.moved = 0
         
-        self.perception = pygame.Rect(0,0,3*TILE_SIZE, 3*TILE_SIZE)
-        self.perception = self.perception.scale_by(0.9)
-        self.perception.center = self.rect.center
-        # perception_size talvez
+        self.set_perception(3)
+        
+        self.busy = False
+        self.found_resources = []
+        self.limit = 20
+    
+    def define_path_to(self, position):
+        grid = Grid(matrix=self.game.matrix)
+        
+        offset_x =  - self.game.camera.position[0] + WINDOW_WIDTH / 2
+        offset_y =  - self.game.camera.position[1] + WINDOW_HEIGHT / 2
+        offset = pygame.Vector2((offset_x, offset_y))
+        
+        start = grid.node(round(self.rect.center[0]/TILE_SIZE), round(self.rect.center[1]/TILE_SIZE)) 
+        end = grid.node(round(position[0]), round(position[1]))
+        
+        finder = AStarFinder()
+        
+        path, runs = finder.find_path(start, end, grid)
+        
+        moves = []
+        for move in path:
+            moves.append(pygame.Vector2((move.x, move.y)))
+        
+        directions = []
+        for i in range(len(moves) - 1):
+            direction = moves[i + 1] - moves[i]
+            direction = direction.normalize()
+            directions.append(direction)
+        
+        self.moves = directions
 
-    def input(self):
+    def fuck_around(self):
+            
         if pygame.mouse.get_pressed()[0]:
             offset_x =  - self.game.camera.position[0] + WINDOW_WIDTH / 2
             offset_y =  - self.game.camera.position[1] + WINDOW_HEIGHT / 2
             offset = pygame.Vector2((offset_x, offset_y))
-            
             mouse_position = pygame.Vector2(pygame.mouse.get_pos())
-            self_center = pygame.Vector2(self.rect.center)
-            
-            # print(mouse_position - offset, self_center)
-            distance = (mouse_position - offset - self_center) /TILE_SIZE
-            
-            print(round(distance[0]), round(distance[1]))
-            
-        if len(self.moves) > 0:
-            return
-        keys = pygame.key.get_pressed()
-        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
-        
-        if self.direction.x > 0:
-            self.moves.append(pygame.Vector2(1,0))
-        elif self.direction.x < 0:
-            self.moves.append(pygame.Vector2(-1,0))
-        elif self.direction.y > 0:
-            self.moves.append(pygame.Vector2(0,1))
-        elif self.direction.y < 0:
-            self.moves.append(pygame.Vector2(0,-1))
-            
-        self.direction = self.direction.normalize() if self.direction else self.direction
-        
+            mouse_position = (mouse_position - offset)/TILE_SIZE
+    
+            self.define_path_to(mouse_position)
+    
+    def set_perception(self, size):
+        self.perception_size = size
+        self.perception = pygame.Rect(0,0,self.perception_size*TILE_SIZE, self.perception_size*TILE_SIZE)
+        self.perception = self.perception.scale_by(0.9)
+        self.perception.center = self.rect.center
+        pass
+    
+    def define_target(self):
+        pass
+    
     def move(self, delta_time):
-        
         if not self.moving:
             if len(self.moves) > 0:
-                print(self.moves)
                 self.direction = self.moves[0]
                 self.moving = True
+            else:
+                if len([resource for resource in self.found_resources if (pygame.time.get_ticks() - resource.last_tried) > resource.cooldown_duration]) > 0:
+                    self.define_target()
+                else:
+                    self.fuck_around()
         else:
-            self.hitbox.x += self.direction.x * self.speed * delta_time
-            self.hitbox.y += self.direction.y * self.speed * delta_time
-            # self.collission('vertical')
+            self.rect.x += self.direction.x * self.speed * delta_time
+            self.rect.y += self.direction.y * self.speed * delta_time
             
             self.moved += self.speed * delta_time
             
             if self.moved >= 64:
+                overshoot = self.moved - 64
+                self.rect.x -= self.direction.x * overshoot
+                self.rect.y -= self.direction.y * overshoot
+                
                 self.moves.remove(self.moves[0])
+                self.direction = pygame.Vector2((0,0))
                 self.moving = False
                 self.moved = 0
-        
+
         self.collission('horizontal')
-        self.rect.center = self.hitbox.center
         self.perception.center = self.rect.center
+    
+    
+    def found_resource(self, resource):
+        if not resource in self.found_resources:
+            self.found_resources.append(resource)
     
     def collission(self, direction):
         for sprite in self.collision_sprites:
             if sprite.rect.colliderect(self.perception): ## diferenciar de recurso depois
-                sprite.disapearing = True
-                    
+                if isinstance(sprite, FogOfWar):
+                    sprite.disapearing = True
+                if isinstance(sprite, Resource):
+                    if sprite.value <= self.limit:
+                        self.found_resource(sprite)
                         
         pass
     
+    def speed_control(self):
+        keys = pygame.key.get_just_pressed()
+        
+        if int(keys[pygame.K_w]):
+            self.speed += 100
+        elif int(keys[pygame.K_s]):
+            self.speed -= 100
+        
     def update(self, delta_time):
-        self.input()
+        # self.fuck_around()
+        m_pos_x = int(self.rect.topright[0]/TILE_SIZE)
+        m_pos_y = int(self.rect.bottomright[1]/TILE_SIZE)
+        self.m_position = (m_pos_x, m_pos_y)
         self.move(delta_time)
         self.animate(delta_time)
+        self.speed_control()
+        # self.game.camera.position = pygame.Vector2((self.rect.center))
     
     def load_images(self):
         self.display_surface = pygame.display.get_surface()
