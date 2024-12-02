77bdb80257c52f2b47914b79ace9f49a7250dfd2
levi-carvalho <lv.levicarvalho@gmail.com>
2024-12-01 22:16:39 -0300

    improved the logic for the agents to work together

--- code/agents/utility_based.py
+++ code/agents/utility_based.py
@@ -2,69 +2,72 @@ from settings import *
 from agents.objective_based import ObjectiveBased
 
 class UtilityBased(ObjectiveBased):
-    def __init__(self, size, position, sprite_sheet, groups, collision_sprites, objectives, utility_agents):
-        super().__init__(size, position, sprite_sheet, groups, collision_sprites, objectives)
+    def __init__(self, size, position, sprite_sheet, utility_agents, game):
+        super().__init__(size, position, sprite_sheet, game)
         self.colleagues = utility_agents
-        self.objective = None
         self.limit = 50
         self.waiting = False
+        self.set_perception(3)
+        utility_agents.add(self)
+        
+        # if leader if leader moving left, self.left = leader righr, etc 
+    def define_resources_priority(self):
+        current_time = pygame.time.get_ticks()
+        self.found_resources = sorted(self.found_resources, key=lambda r: (not(current_time - r.last_tried) > r.cooldown_duration, self.calculate_moves(self.m_position, r.m_position) + self.calculate_moves(self.game.base.m_position, r.m_position)))
+        return
     
-    def calc_objective(self):
-        objectives = sorted(self.objectives, key = lambda objective: math.dist(objective.rect.center, self.rect.center))
-        objectives = [objective for objective in objectives if (pygame.time.get_ticks() - objective.last_tried) >= objective.cooldown_duration]
-        return objectives
-    
-    def calc_next_target(self):
-        if self.waiting and self.objective:
-            print(self.waiting, "waitt", pygame.time.get_ticks(), self.rect.center)
-            print('objective = ', self.objective.groups())
-            
-            found_colleague = False
-            for colleague in self.colleagues:
-                if colleague.objective == self.objective:
-                    if colleague != self:
-                        found_colleague = True
-                        if found_colleague == True and colleague.waiting == True:
-                            self.waiting = False
-                            colleague.waiting = False
-                        print("truou")
-                        break
-            if not found_colleague:
-                self.waiting = False
-                self.objective.last_tried = pygame.time.get_ticks()                        
-            
-            if not self.objective or len(self.objective.groups()) == 0 or self.objective.holders > 1:
-                self.waiting = False
-            
+    def take_resource(self):
+
+        if not self.found_resources:
+            print("Nenhum recurso encontrado.")
+            self.busy = False
             return
-        
-        objectives = self.calc_objective()
-        
-        while objectives[self.objective_index].holder:
-            self.objective_index += 1
-        
-        self.objective = objectives[self.objective_index]
-        self.target_rect = self.objective.rect
-        self.objective_index = 0
-        
-        self.calc_direction()
-    
-    def carry(self, sprite):
-        if sprite.value == 50:
-            for colleague in self.colleagues:
-                if colleague.objective == self.objective:
+
+        resource = self.found_resources[0]
+        if resource.holder:
+            print("ja holde")
+            self.found_resources.remove(resource)
+            self.busy = False
+            return
+
+        found_colleague = False
+        current_time = pygame.time.get_ticks()
+        if resource.value == self.limit:
+            if (current_time - resource.last_tried) > resource.cooldown_duration:
+                for colleague in self.colleagues:
                     if colleague != self:
-                        self.waiting = True
-                        if math.dist(colleague.rect.center, self.rect.center) < 30:
-                            self.set_returning(sprite, colleague)
-                            self.set_returning(sprite, self)
-                            self.waiting = False
+                        if not resource in colleague.found_resources:
+                            colleague.found_resources.append(resource)
+                            print("colocou no do colega")
+                        else:
+                            print("não colocou no do colega")
                             
-            if not self.waiting:
-                sprite.last_tried = pygame.time.get_ticks()
-                self.waiting = False
-                
-            
+                        print('ai', pygame.time.get_ticks())             
+                        if colleague.found_resources[0] == resource:
+                            found_colleague = True
+                            print("esperando")
+                            if colleague.m_position == self.m_position:
+                                    
+                                self.define_path_to(self.game.base.m_position)
+                                self.returning = True
+                                resource.holder = self
+
+                                colleague.define_path_to(self.game.base.m_position)
+                                colleague.returning = True
+                                colleague.resource.holder = self
+                                break
+                            
+                if not found_colleague:
+                    print("Não encontrou colega")
+                    resource.last_tried = current_time
+                    self.fuck_around()
+                    # self.define_path_to(self.m_position)
+                    self.busy = False
+                    return
+
+        else:
+            print('pegou')
+            self.define_path_to(self.game.base.m_position)
+            self.returning = True
+            resource.holder = self
             return
-        if not self.busy and not sprite.holder:
-            self.set_returning(sprite, self)
