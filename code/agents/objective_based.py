31698bd8fc373649807cf07857a0b41408e006cd
levi-carvalho <lv.levicarvalho@gmail.com>
2024-12-01 22:14:54 -0300

    define the closest resource as a target

--- code/agents/objective_based.py
+++ code/agents/objective_based.py
@@ -1,41 +1,19 @@
 from settings import *
-from agents.simple_reactive import SimpleReactive
+from agents.state_based import StateBased
 
-class ObjectiveBased(SimpleReactive):
-    def __init__(self, size, position, sprite_sheet, groups, collision_sprites, objectives):
-        super().__init__(size, position, sprite_sheet, groups, collision_sprites)
-        self.objectives = objectives        
-        self.objective_index = 0
-    
-    
-    def calc_direction(self):
-        best_direction = None
-        min_distance = float('inf')
+class ObjectiveBased(StateBased):
+    def __init__(self, size, position, spritesheet, game):
+        super().__init__(size, position, spritesheet, game)
+        self.set_perception(1)
         
-        for direction in self.directions:
-            next_position = self.rect.center + direction
-            distance_to_target = math.dist(self.target_rect.center,next_position)
-
-            if distance_to_target < min_distance:
-                min_distance = distance_to_target
-                best_direction = direction
-
-        self.target_rect = self.hitbox_rect.move(best_direction.x, best_direction.y)
+    def calculate_moves(self, start, end):
+        grid = Grid(matrix=self.game.matrix)
+        start = grid.node(start[0], start[1])
+        end = grid.node(end[0], end[1])
+        finder = AStarFinder()
         
-    def calc_next_target(self):
+        path, runs = finder.find_path(start, end, grid)
+        return len(path)
         
-        objectives = sorted(self.objectives, key = lambda objective: math.dist(objective.rect.center, self.rect.center))
-        objectives = [sprite for sprite in objectives if sprite.value < 50]
-        
-        while objectives[self.objective_index].holder:
-            self.objective_index += 1
-        
-        self.objective = objectives[self.objective_index]
-        self.target_rect = self.objective.rect
-        self.objective_index = 0
-        
-        self.calc_direction()
-        
-    
-    def set_new_target(self):
-        self.calc_next_target()
+    def define_resources_priority(self):
+        self.found_resources = sorted(self.found_resources, key= lambda resource: self.calculate_moves(self.m_position, resource.m_position))
